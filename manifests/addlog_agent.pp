93f10e42acfee79929f9fefcf8f6932de2642b7b
Ante Jurjevic <ante.jurjevic@srce.hr>
2021-04-29 12:39:31 +0200

    Add addlog for manager

--- manifests/addlog_agent.pp
+++ manifests/addlog_agent.pp
@@ -1,6 +1,6 @@
 # Wazuh App Copyright (C) 2021 Wazuh Inc. (License GPLv2)
 #Define a log-file to add to ossec
-define wazuh::addlog(
+define wazuh::addlog_agent(
   $logfile      = undef,
   $logtype      = 'syslog',
   $logcommand   = undef,
